d976b2607032b45815e2b4775c23a6ed589a95e7
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-12 23:54:43 +0900

    [Add] ライブラリを追加（Exposed, AnvilGUI, SmartInvsR, VaultAPI）

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,22 @@ repositories {
     maven ("https://papermc.io/repo/repository/maven-public/")
     maven  ("https://oss.sonatype.org/content/groups/public/")
     maven ("https://jitpack.io")
+    maven ("https://repo.codemc.io/repository/maven-snapshots/")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
 
+    val exposedVersion = "0.39.2"
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    implementation("net.wesjd", "anvilgui", "1.5.3-SNAPSHOT")
+    implementation("com.github.M1n1don", "SmartInvsR", "2.0.0")
     implementation("com.github.hazae41", "mc-kutils", "master-SNAPSHOT")
 
+    compileOnly("com.github.MilkBowl", "VaultAPI", "1.7")
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
 
