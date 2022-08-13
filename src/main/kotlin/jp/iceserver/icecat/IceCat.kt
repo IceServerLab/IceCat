b7df70c738063e088f24fec1165d8b23355c6880
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-13 11:32:15 +0900

    [Add] Vaultを読み込み

--- src/main/kotlin/jp/iceserver/icecat/IceCat.kt
+++ src/main/kotlin/jp/iceserver/icecat/IceCat.kt
@@ -1,14 +1,35 @@
 package jp.iceserver.icecat
 
+import net.milkbowl.vault.economy.Economy
+
+
 class IceCat : AbstractIceCat()
 {
     companion object
     {
         lateinit var plugin: IceCat
+        lateinit var economy: Economy
     }
 
     override fun onEnable()
     {
         plugin = this
+
+        if (!setupEconomy())
+        {
+            logger.severe(String.format("Vaultの依存関係が見つかりません！", description.name));
+            server.pluginManager.disablePlugin(this);
+            return;
+        }
+    }
+
+    private fun setupEconomy(): Boolean
+    {
+        if (server.pluginManager.getPlugin("Vault") == null) return false
+
+        val rsp = server.servicesManager.getRegistration(Economy::class.java) ?: return false
+        economy = rsp.provider
+
+        return true
     }
 }
