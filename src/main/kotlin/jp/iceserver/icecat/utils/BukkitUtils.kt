d20dceb4dbe236b420afcf3f3d2bb80dc9a5af82
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-15 10:30:25 +0900

    [Change] オフラインプレイヤーに変更

--- src/main/kotlin/jp/iceserver/icecat/utils/BukkitUtils.kt
+++ src/main/kotlin/jp/iceserver/icecat/utils/BukkitUtils.kt
@@ -3,11 +3,11 @@ package jp.iceserver.icecat.utils
 import net.kyori.adventure.text.Component
 import org.bukkit.ChatColor
 import org.bukkit.Material
-import org.bukkit.entity.Player
+import org.bukkit.OfflinePlayer
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 
-fun Player.getPlayerHead(name: String): ItemStack
+fun OfflinePlayer.getPlayerHead(name: String): ItemStack
 {
     val itemStack = ItemStack(Material.PLAYER_HEAD)
     val skull = itemStack.itemMeta as SkullMeta
