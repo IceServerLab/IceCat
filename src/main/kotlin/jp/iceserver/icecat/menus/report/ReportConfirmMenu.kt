5bb5d5657661d75c85ccf6cc4f8da2a0608682ff
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-09-14 23:27:46 +0900

    [Add] Webhookにレポート内容を送信

--- src/main/kotlin/jp/iceserver/icecat/menus/report/ReportConfirmMenu.kt
+++ src/main/kotlin/jp/iceserver/icecat/menus/report/ReportConfirmMenu.kt
@@ -1,17 +1,22 @@
 package jp.iceserver.icecat.menus.report
 
+import club.minnced.discord.webhook.WebhookClient
+import club.minnced.discord.webhook.send.WebhookEmbed
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder
+import club.minnced.discord.webhook.send.WebhookMessageBuilder
 import dev.m1n1don.smartinvsr.inventory.ClickableItem
 import dev.m1n1don.smartinvsr.inventory.SmartInventory
 import dev.m1n1don.smartinvsr.inventory.content.InventoryContents
 import dev.m1n1don.smartinvsr.inventory.content.InventoryProvider
 import hazae41.minecraft.kutils.bukkit.msg
 import jp.iceserver.icecat.IceCat
-import jp.iceserver.icecat.config.MainConfig
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.OfflinePlayer
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+import java.text.SimpleDateFormat
+import java.time.Instant
 
 class ReportConfirmMenu(private val target: OfflinePlayer, private val reasons: List<String>) : InventoryProvider
 {
@@ -43,10 +48,30 @@ class ReportConfirmMenu(private val target: OfflinePlayer, private val reasons: 
         }, {
             val builder = StringBuilder()
             reasons.forEach { builder.append("${it.replace("§r", "")}, ") }
-            builder.setLength(builder.length - 2)
 
             player.playSound(player.location, Sound.BLOCK_NOTE_BLOCK_PLING, 30f, 1f)
             player.msg("&2>&a> レポートを送信しました！\n&7(対象者: ${target.name}, 理由: ${builder})")
+
+            val client = WebhookClient.withUrl("https://discord.com/api/webhooks/1019605403497148477/XeIlBu2DL5x7Z_cH2knWLQUOv7B6UgzhAK9BkUSzzK7f7J6f1pDtXXObJ1lRCsClWAVv")
+
+            val embedBuilder = WebhookEmbedBuilder()
+            embedBuilder.setTitle(WebhookEmbed.EmbedTitle("レポートが届きました。", ""))
+            embedBuilder.addField(WebhookEmbed.EmbedField(true, "報告者", player.name))
+            embedBuilder.addField(WebhookEmbed.EmbedField(true, "対象者", target.name.toString()))
+            embedBuilder.addField(WebhookEmbed.EmbedField(false, "座標", "${player.location.x} ${player.location.y} ${player.location.z}"))
+            embedBuilder.addField(WebhookEmbed.EmbedField(false, "理由", builder.toString()))
+            embedBuilder.addField(WebhookEmbed.EmbedField(false, "詳細", "なし"))
+            embedBuilder.setColor(0xa047ff)
+            embedBuilder.setThumbnailUrl("https://cravatar.eu/avatar/${target.name}/100")
+            embedBuilder.setTimestamp(Instant.ofEpochMilli(System.currentTimeMillis()))
+
+            val messageBuilder = WebhookMessageBuilder()
+            messageBuilder.setUsername("IceServer Report")
+            messageBuilder.setAvatarUrl("https://images-ext-2.discordapp.net/external/q35szkJdGNEokxVLNou1mnKqbFUIquR2O5xNBN3seu4/https/cdn.discordapp.com/icons/680934020992729090/7e7ba969e7f047b66caaf66493f0b3b3.png")
+            messageBuilder.addEmbeds(embedBuilder.build())
+
+            client.send(messageBuilder.build())
+
             inventory(player, listOf()).close(player)
         }))
 
