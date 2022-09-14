fd4a859934cbe96cec48cba19d6d2475f113413f
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-09-14 22:44:20 +0900

    [Add] コンフィグにプレフィックスを追加

--- src/main/kotlin/jp/iceserver/icecat/config/MainConfig.kt
+++ src/main/kotlin/jp/iceserver/icecat/config/MainConfig.kt
@@ -4,5 +4,6 @@ import hazae41.minecraft.kutils.bukkit.PluginConfigFile
 
 object MainConfig : PluginConfigFile("config")
 {
+    var prefix by string("prefix")
     var reportContents by stringList("reportContents")
 }
