22a3880b3d75c702ca754cfc4ea33ce46165e1ab
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-09-14 21:53:06 +0900

    [Change] プレイヤーヘッド取得のItemStackを少し改良

--- src/main/kotlin/jp/iceserver/icecat/utils/Utils.kt
+++ src/main/kotlin/jp/iceserver/icecat/utils/Utils.kt
@@ -9,12 +9,12 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
 import kotlin.math.floor
 
-fun OfflinePlayer.getPlayerHead(name: String): ItemStack
+fun OfflinePlayer.getPlayerHead(): ItemStack
 {
     val itemStack = ItemStack(Material.PLAYER_HEAD)
     val skull = itemStack.itemMeta as SkullMeta
 
-    skull.displayName(Component.text(ChatColor.translateAlternateColorCodes('&', name)))
+    skull.displayName(Component.text("${ChatColor.GOLD}${this.name ?: "Unknown"}"))
     skull.owningPlayer = this
     itemStack.itemMeta = skull
 
